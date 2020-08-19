f772b2ec1ecdb2e62f94fe1aaf6ff1e72892e747
domavaem <domavaem@gmail.com>
2020-08-19 10:42:44 +0900

    Create: init files

---
+++ client.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "regenerator-runtime/runtime"; //import regeneratorRuntime from "regenerator-runtime";
+
+ReactDom.render(
+  <>
+    <div>Hello, GreenTimer!</div>
+  </>,
+  document.querySelector("#root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  name: "greentimer",
+  mode: "development", // development/production
+  devtool: "eval", // eval/hidden-source-map
+  entry: {
+    app: ["./client"],
+  },
+  output: {
+    filename: "app.js",
+    path: path.join(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  resolve: {
+    extensions: [".js", ".json", ".jsx", ".css"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+          compact: true,
+        },
+      },
+    ],
+  },
+};
